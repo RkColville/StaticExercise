c25f4d94f4714c451f13feebaf383fb2761f5260
Robert Colville <rkcolville@gmail.com>
2021-06-10 10:05:59 -0500

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Calling Fahrenheit to Celsius temp converter method:
+
+            var celsius = TempConverter.FahrenheitToCelsius(68);
+            Console.WriteLine($"Hello, the current degrees in Celsius is {celsius}");
+
+            // Calling Celsius to Fahrenheit temp converter method:
+            var Fahrenheit = TempConverter.CelsiusToFahrenheit(20);
+            Console.WriteLine($"Hello, the current degrees in fahrenheit is {Fahrenheit}");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        //Create a method to convert Fahrenheit to Celsius:
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            var result = (fahrenheit - 32) / 1.8;
+            return result;
+        }
+        // Create a method to convert Celsius to Fahrenheit:
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 9) / 5 + 32;
+        }
+    }
+}
